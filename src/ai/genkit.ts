4bae402effda6d6e42f5d7b0d44e0223db7e99be
Yecheskel Maor <ymzpe.net@gmail.com>
2025-07-05 23:37:03 +0000

    the app is not starting ngrok

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -7,6 +7,9 @@ export const ai = genkit({
       client: {
         host: 'https://81af-2a06-c701-9364-c400-a6f1-4b98-216b-5b28.ngrok-free.app',
         requestTimeout: 120000, // 2 minutes
+        headers: {
+          'ngrok-skip-browser-warning': 'true',
+        },
       },
     }),
   ],
