c35a1330f2fefd29f4580623d8953a3be114ede9
Yecheskel Maor <ymzpe.net@gmail.com>
2025-07-07 18:26:41 +0000

    ngrok

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {ollama} from 'genkitx-ollama';
 export const ai = genkit({
   plugins: [
     ollama({
-      serverAddress: 'https://b308-2a0d-6fc2-6800-6600-7a73-4abf-f3aa-67cb.ngrok-free.app',
+      serverAddress: 'https://7545-2a0d-6fc2-6800-6600-390b-5bea-4875-a81d.ngrok-free.app',
       client: {
         requestTimeout: 120000, // 2 minutes
         headers: {
