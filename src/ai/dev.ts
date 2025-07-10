08cf2647bcb104896dc8ff9058b88e43ccd1e219
Yecheskel Maor <ymzpe.net@gmail.com>
2025-07-10 11:18:09 +0000

    AND WHY DO INEED A GEMININ KEY?

--- src/ai/dev.ts
+++
@@ -1,2 +0,0 @@
-// This file is no longer used and can be deleted.
-// The Genkit dev server is not needed for the current setup.
--- src/ai/genkit.ts
+++
@@ -1,2 +0,0 @@
-// This file is no longer used and can be deleted.
-// The application now communicates directly with the custom Python Golem server.
