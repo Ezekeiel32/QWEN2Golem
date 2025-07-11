e0aa386fda68d800fc33bebf99e27506443fc9ce
Chezy <receptorcb1@gmail.com>
2025-07-11 18:56:49 +0300

    Feat: Create use-sidebar hook to fix Vercel build

---
+++ src/hooks/use-sidebar.tsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { SidebarContext } from '@/components/ui/sidebar';
+
+export function useSidebar() {
+  const context = useContext(SidebarContext);
+  if (context === undefined) {
+    throw new Error('useSidebar must be used within a SidebarProvider');
+  }
+  return context;
+} 
